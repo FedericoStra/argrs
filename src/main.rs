07164485454afb8a46bd361f2ba1cc2c4b0817a2
Federico Stra <stra.federico@gmail.com>
2023-04-21 12:10:05 +0200

    Clean up the code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 fn main() {
     let args = std::env::args();
     let argc = args.len();
-    let width: usize;
-    if argc <= 10 {
-        width = 1
+
+    // compute the width to print numbers in the range `0..argc`
+    let width: usize = if argc <= 10 {
+        1
     } else if argc <= 100 {
-        width = 2
+        2
     } else if argc <= 1_000 {
-        width = 3
+        3
     } else if argc <= 10_000 {
-        width = 4
+        4
     } else if argc <= 100_000 {
-        width = 5
+        5
     } else if argc <= 1_000_000 {
-        width = 6
+        6
     } else {
-        width = 0
-    }
+        0
+    };
+
     for (i, arg) in args.enumerate() {
-        println!("{:width$}: `{}`", i, arg, width = width);
+        println!("{:width$}: `{}`", i, arg);
     }
 }
